e4b475ac565cd2d90aae8b16ee495b459099d043
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-01-05 06:31:05 +0530

    Product add logic done

---
+++ Back-End/Controller/product-controller.js
@@ -0,0 +1,35 @@
+const catchAsync = require("../utils/catchAync");
+const appError = require("../utils/appError");
+const Product = require("../Models/Product");
+
+const addProduct = catchAsync(async(req, res, next)=>{
+    const {artNo, brand, price, category, color, stock } = req.body;
+
+    const product = await Product.findOne({artNo});
+
+    if(product){
+        return(next(new appError("this product is already exist...",403)));
+    }
+
+    const newProduct = new Product(
+        {
+            artNo,
+            brand,
+            price,
+            category,
+            color,
+            stock
+        }
+    )
+    await newProduct.save();
+
+    res.status(201).json({
+        status: "success",
+        message: "Product added successfully",
+        data: {
+            product: newProduct
+        }
+    });
+})
+
+module.exports = {addProduct}
--- Back-End/server.js
+++ Back-End/server.js
@@ -7,6 +7,7 @@ const globalErrorHandler = require('./Controller/errorController');
 const AppError = require('./utils/appError');
 
 const authRoutes = require('./routes/auth-routes');
+const productRoutes = require("./routes/product-routes")
 
 
 const app = express();
@@ -27,6 +28,7 @@ app.use(
 
 app.use(express.json({ limit: "10kb" }));
 app.use('/api/auth', authRoutes);
+app.use("/api/product",productRoutes);
 
 
 app.use(globalErrorHandler);
